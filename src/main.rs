93725f001c37b906c728368530969f5ac2e14bd3
linkdrone <fengwanqing2014@gmail.com>
2022-12-05 00:38:08 +0800

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn demo01() {
     let a: i32 = 6;
 
     if a <= 6 {
@@ -6,4 +6,16 @@ fn main() {
     } else if a > 7 {
         println!("a > 7: {a}");
     }
+}
+
+fn demo02() {
+    for number in (1..4).rev() {
+        println!("demo02 number: {number}")
+    }
+}
+
+fn main() {
+    demo01();
+
+    demo02();
 }
