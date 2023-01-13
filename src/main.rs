c973a7fb60534e38ba178db85e8440627759e1b2
linkdrone <fengwanqing2014@gmail.com>
2023-01-13 19:10:07 +0800

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
 use rand::*;
 
-fn demo01() {
-    let a: i32 = 6;
-    let b = 32;
-
-    println!("b: {b}");
-
-    if a <= 6 {
-        println!("a <= 6: {a}");
-    } else if a > 7 {
-        println!("a > 7: {a}");
-    }
-}
-
 fn demo02() {
     for number in (1..4).rev() {
         println!("demo02 number: {number}");
@@ -73,8 +60,6 @@ fn coin_to_usd(coin: Coin) -> u32 {
 }
 
 fn main() {
-    demo01();
-
     demo02();
 
     demo03();
---
+++ tests/demo.rs
@@ -0,0 +1,13 @@
+#[test]
+fn demo01() {
+    let a: i32 = 6;
+    let b = 32;
+
+    println!("b: {b}");
+
+    if a <= 6 {
+        println!("a <= 6: {a}");
+    } else if a > 7 {
+        println!("a > 7: {a}");
+    }
+}
