0d0e37cd3cccf539cad68ab2b814ca85d492ee28
linkdrone <fengwanqing2014@gmail.com>
2023-01-18 14:35:00 +0800

    updates

--- tests/demo.rs
+++ tests/demo.rs
@@ -1,6 +1,6 @@
 #[test]
 fn demo01() {
-    let a: i32 = 6;
+    let a = 6;
     let b = 32;
 
     println!("b: {b}");
@@ -9,5 +9,19 @@ fn demo01() {
         println!("a <= 6: {a}");
     } else if a > 7 {
         println!("a > 7: {a}");
+    }
+}
+
+#[test]
+fn demo02() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
     }
 }
